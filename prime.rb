1484f1ea1b5e22f5c958aa57e11a8433f356119b
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-11 22:54:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@
 
 
 def prime?(int)
-  start = 2
+  initial = 2
   if int > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+    range = (initial..int-1).to_a
+    range.none? do |test| #none of the numbers should return true for none? to return true. If none of the numbers
                                 # return true, then they are not divisible by the number and the number is prime
-      number % num_to_test == 0
+      int % test == 0
     end
   else
     false
